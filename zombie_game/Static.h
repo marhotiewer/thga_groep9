2fa38e0c1fb097e8026f756d941869400bab2335
pim12345 <60601873+pim12345@users.noreply.github.com>
2021-01-19 15:14:55 +0100

    added begin doxygen on 2 files

--- zombie_game/Static.h
+++ zombie_game/Static.h
@@ -4,9 +4,18 @@
 #include "AssetManager.h"
 #include "Drawable.h"
 
+/// \class Static
+/// \brief
+/// Class for the Static items
+/// \details
+/// The Static class inhert from the Drawable class.
 class Static : public Drawable
 {
 	public:
+		/// <summary>
+		/// The static class need the AssetManager for textures
+		/// </summary>
+		/// <param name="AssetManager"></param>
 		Static(AssetManager& assets) : Drawable(assets) { this->type = Type::Static; }
 };
 
--- zombie_game/Tree.h
+++ zombie_game/Tree.h
@@ -5,10 +5,29 @@
 
 #include "Static.h"
 
+/// \class Tree
+/// \brief
+/// Tree Class
+/// \details
+/// The Tree Class is a class for the Trees in the game. 
+/// All functions are inherited from Static, and Static inherited it's functions from Drawable. 
+/// The function getHitbox is overrided because the hitbox is different because the Player and Zombie can walk behind a tree.
+/// The Texture used for the Tree is the TreeTexture and is loaded from the AssetManager class. 
 class Tree : public Static
 {
 public:
+	/// <summary>
+	/// The Tree class need the AssetManager for the tree texture. 
+	/// The Tree class needs a position to place the tree in the correct position.
+	/// </summary>
+	/// <param name="AssetManager"></param>
+	/// <param name="position"></param>
 	Tree(AssetManager& assets, sf::Vector2f pos);
+	/// <summary>
+	/// The function return a SFML hitbox for the Tree. 
+	/// This hitbox differed from the inherited hitbox function from Static. Because the Player and the Zombie can walk behind a tree. 
+	/// </summary>
+	/// <returns>a SFML FloatRect (sf::FloatRect) with the correct hitbox of the tree.</returns>
 	sf::FloatRect getHitbox() override;
 };
 
