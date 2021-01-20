ccf8cd5ab2751443aa6c6bd853b31250b71d84dc
pim12345 <60601873+pim12345@users.noreply.github.com>
2021-01-20 10:36:17 +0100

    update drawable doxygen init

--- zombie_game/Drawable.h
+++ zombie_game/Drawable.h
@@ -17,30 +17,77 @@
 class Drawable
 {
 	public:
-		/// \enum
+		/// \enum Type
+		/// /brief Enum that state the state of that drawable, like it is a Static item or a Zombie, etc. 
 		enum class Type
 		{
-			Projectile,
-			Player,
-			Static,
-			Enemy,
-			Empty
+			Projectile,///<Can things be like a bullet. 
+			Player,///<The player is the player on the screen.
+			Static,///<Static items are items that can't move. 
+			Enemy,///<Enemy's must be beaten by the player.
+			Empty///<Everything other than the other types.
 		};
+		/// <summary>
+		/// The Drawable required a AssetManager. 
+		/// </summary>
+		/// <param name="assets"></param>
 		Drawable(AssetManager& assets) : assets(assets) {}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="drawable"></param>
+		/// <param name="delta"></param>
+		/// <returns></returns>
 		virtual bool isColliding(Drawable& drawable, sf::Vector2f delta = sf::Vector2f(0.f, 0.f));
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="window"></param>
+		/// <param name="deltaTime"></param>
 		virtual void update(sf::RenderWindow* window, float deltaTime) {}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="window"></param>
 		virtual void debug_draw(sf::RenderWindow* window);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="window"></param>
 		virtual void draw(sf::RenderWindow *window);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		virtual sf::FloatRect getHitbox();
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="dmg"></param>
 		virtual void damage(int dmg) {};
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		virtual sf::Vector2i getSize();
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		virtual sf::Vector2f getPos();
+		/// <summary>
+		/// 
+		/// </summary>
 		Type type = Type::Empty;
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		bool isActive();
 	protected:
-		AssetManager& assets;
-		bool active = true;
-		sf::Sprite sprite;
+		AssetManager& assets;///<
+		bool active = true;///<
+		sf::Sprite sprite;///<
 };
 
 #endif
