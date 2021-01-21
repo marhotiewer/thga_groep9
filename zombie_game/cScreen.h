49de6e1482eac0959d9f6699f0b8828ac0316fb5
pim12345 <60601873+pim12345@users.noreply.github.com>
2021-01-21 11:58:30 +0100

    added doxygen to button and cScreen

--- zombie_game/cScreen.h
+++ zombie_game/cScreen.h
@@ -1,17 +1,29 @@
 #ifndef CSCREEN_H
 #define CSCREEN_H
+/// @file
 
 enum class Screen {
-	MainMenu,
-	Game,
-	OptionsMenu,
-	PauseMenu,
-	Scores,
-	None
+	MainMenu,///< Startup screen.
+	Game,///< The screen with the game.
+	OptionsMenu,///< Screen with options.
+	PauseMenu,///< Menu that pause the game.
+	Scores,///< Screen with scores
+	None///< None is exit the application.
 };
 
+
+/// \class cScreen
+/// \brief
+/// Head class for all different screens. 
+/// \details
+/// This class is used for screen switching. 
+/// classes that inhert from this class, need to have a run() function.
 class cScreen {
 	public:
+		/// <summary>
+		/// Function that contains the loop of the window. 
+		/// </summary>
+		/// <returns>The screen to switch to from enum type screen</returns>
 		virtual Screen run() = 0;
 };
 
